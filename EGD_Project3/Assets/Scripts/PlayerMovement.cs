ffa02d6354ed89bd9c9a26410ad7815fab586b66
CocoTang03 <qinwentang@outlook.com>
2023-09-26 02:29:56 -0400

    Fixed a few bugs; last update before sleep
    will work on it tomorrow all day.

--- EGD_Project3/Assets/Scripts/PlayerMovement.cs
+++ EGD_Project3/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public AudioSource footstep;
     [SerializeField] float speed = 10f;
+    [SerializeField] float minMoveDistance = 0.1f;
     public Transform orientation;
 
     private float horizontalInput;
     private float verticalInput;
+
+    private Vector3 previousPosition;
 
     Vector3 moveDirection;
     Rigidbody rb;
@@ -18,17 +21,15 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        //rb = GetComponent<Rigidbody>();
+        //rb.freezeRotation = true;
+        previousPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void FixedUpdate()
+
+    private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -37,5 +38,20 @@ public class PlayerMovement : MonoBehaviour
 
         //rb.AddForce(moveDirection.normalized * speed, ForceMode.Force);
         transform.Translate(moveDirection.normalized * speed * Time.deltaTime);
+
+        float moveDistance = Vector3.Distance(transform.position, previousPosition);
+        if (moveDistance >= minMoveDistance)
+        {
+            //Debug.Log("GET into");
+            playFootsteps(footstep);
+            previousPosition = transform.position;
+        }
+    }
+
+    private IEnumerator playFootsteps(AudioSource footstep)
+    {
+        footstep.Play();
+        yield return new WaitForSeconds(footstep.clip.length);
+        
     }
 }
